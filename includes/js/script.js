144339b0975ea003a2402b18d83446bc806b3aab
Fadzli Razali <fadzlitown@gmail.com>
2015-01-14 17:29:40 +0800

    Done desginer-register page

--- includes/js/script.js
+++ includes/js/script.js
@@ -26,4 +26,27 @@ $(function(){
         e.preventDefault();
         $('#successAlert').slideDown();
     });
+
+
+    /*not working yet, smooth scrolling */
+    $("ul li <a href='#'>").on('click', function(e) {
+
+        // prevent default anchor click behavior
+        e.preventDefault();
+
+        // store hash
+        var hash = this.hash;
+
+        // animate
+        $('html, body').animate({
+            scrollTop: $(this.hash).offset().top
+        }, 300, function(){
+
+            // when done, add hash to url
+            // (default click behaviour)
+            window.location.hash = hash;
+        });
+
+    });
+
 });
